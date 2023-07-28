73055d44fc54e344fd62624fbb415a4ec9e25bfd
b1ron <goldingbyron@gmail.com>
2023-07-28 12:17:50 +0200

    wip

--- pymongo_test.py
+++ pymongo_test.py
@@ -1,9 +1,15 @@
 from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 import sys
 
-CONNECTION_STRING = str(sys.argv[-1])
+# if set in the YAML file
+if str(sys.argv[-1]) == "StableAPI":
+    uri = sys.argv[-2]
+    MongoClient(uri, server_api=ServerApi("1", strict=False, deprecation_errors=True))
+else:
+    uri = sys.argv[-1]
 
-client = MongoClient(CONNECTION_STRING)
+client = MongoClient(uri)
 
 db = client.test
 res = db.command('ping', '1')
