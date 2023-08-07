167133f7f91f75e8c50b17981dcc3a6d51b20abe
b1ron <goldingbyron@gmail.com>
2023-08-07 12:52:13 +0200

    remove stable API settings

--- pymongo_test.py
+++ pymongo_test.py
@@ -2,12 +2,7 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 import sys
 
-# if set in the YAML file
-if str(sys.argv[-1]) == "StableAPI":
-    uri = sys.argv[-2]
-    MongoClient(uri, server_api=ServerApi("1", strict=False, deprecation_errors=True))
-else:
-    uri = sys.argv[-1]
+uri = sys.argv[-1]
 
 client = MongoClient(uri)
 
