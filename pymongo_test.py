de1615e366d0ab6c55d8c125958fc3c83028d0e2
b1ron <80292536+b1ron@users.noreply.github.com>
2023-08-10 14:46:20 +0200

    Extend functionality of the python-example (#2)
    Closes FerretDB/dance#530.

--- pymongo_test.py
+++ pymongo_test.py
@@ -1,11 +1,25 @@
 from pymongo import MongoClient
 import sys
 
-CONNECTION_STRING = str(sys.argv[-1])
+uri = sys.argv[-1]
 
-client = MongoClient(CONNECTION_STRING)
+client = MongoClient(uri)
 
 db = client.test
 res = db.command('ping', '1')
+assert (res == {'ok': 1.0}), "ping failed"
 
-print(res)
+res = db.command('dropDatabase', 1)
+assert (res == {'ok': 1.0}), "dropDatabase failed"
+
+doc_list = [
+    {'_id': 1, 'a': 1},
+    {'_id': 2, 'a': 2},
+    {'_id': 3, 'a': 3},
+    {'_id': 4, 'a': 4},
+]
+
+db['foo'].insert_many(doc_list)
+
+actual = db['foo'].find_one({'a': 4})
+assert (actual == {'_id': 4, 'a': 4}), "Value should be 4"
